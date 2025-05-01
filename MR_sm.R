4fc21b8ad17ccf86f7093e35aa6612e167c39551
Rutuja Gupte <rmgupte@wisc.edu>
2025-05-01 09:54:15 -0500

    final MR files

--- MR_sm.R
+++ MR_sm.R
@@ -4,8 +4,7 @@ library(tidyverse)    # Data wrangling
 library(TwoSampleMR)  # MR 
 library(LDlinkR)      # LD and proxy snps
 
-exp_raw <- fread("sumstats/SM_EUR_QCed_v2.txt")
-exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
+exp_raw <- fread("data/SM_EUR_QCed_v2.txt")
 exp_raw <- as.data.frame(exp_raw)
 
 exp_dat <- format_data( exp_raw,
@@ -13,17 +12,30 @@ exp_dat <- format_data( exp_raw,
                         snp_col = "rsID",
                         beta_col = "BETA",
                         se_col = "SE",
-                        effect_allele_col = "A2",
-                        other_allele_col = "A1",
+                        effect_allele_col = "A1",
+                        other_allele_col = "A2",
                         pval_col = "P",
                         chr_col = "CHR",
                         pos_col = "POS"
 )
 
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+# Perform LD clumping on SNP data, filter SNPs to make it run faster
+clumped_exp <- exp_dat %>% 
+  filter(pval.exposure < 0.01) %>%
+  clump_data(.,
+             clump_kb = 10000,
+             clump_r2 = 0.001,
+             clump_p1 = 1,
+             clump_p2 = 1,
+             pop = "EUR"
+  )
 
+filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+exposure_dat <- filter(clumped_exp, pval.exposure < 1e-05)
 
-out_raw <- fread("sumstats/Age_at_first_birth.tsv")
+# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+
+out_raw <- fread("data/Age_at_first_birth.tsv")
 
 out_raw <- as.data.frame(out_raw)
 
@@ -38,7 +50,7 @@ out_dat <- format_data( out_raw,
 )
 
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -46,13 +58,12 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "exposure/mr_dat_age")
 write.csv(mr_res, "exposure/mr_res_age.csv")
 write.csv(res_single, "exposure/res_single_age.csv")
 
-out_raw <- fread("sumstats/Income.txt")
+out_raw <- fread("data/Income.txt")
 out_raw <- as.data.frame(out_raw)
 
 out_dat <- format_data( out_raw,
@@ -67,7 +78,7 @@ out_dat <- format_data( out_raw,
                 pos_col = "BPos"
 )
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -75,30 +86,29 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "exposure/mr_dat_income")
 write.csv(mr_res, "exposure/mr_res_income.csv")
 write.csv(res_single, "exposure/res_single_income.csv")
 
 
-out_raw <- fread("sumstats/Post-Traumatic_Stress_Disorder.results")
+out_raw <- fread("data/Post-Traumatic_Stress_Disorder.results")
 out_raw <- as.data.frame(out_raw)
-out_raw$BETA <-  (exp(out_raw$OR)/(1+exp(out_raw$OR)))
+out_raw$BETA <-  log(out_raw$OR)
 
 out_dat <- format_data( out_raw,
                   type = "outcome",
                   snp_col = "SNP",
                   beta_col = "BETA",
                   se_col = "SE",
-                  effect_allele_col = "A2",
-                  other_allele_col = "A1",
+                  effect_allele_col = "A1",
+                  other_allele_col = "A2",
                   pval_col = "P",
                   chr_col = "CHR",
                   pos_col = "BP"
 )
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -106,14 +116,13 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "exposure/mr_dat_ptsd")
 write.csv(mr_res, "exposure/mr_res_ptsd.csv")
 write.csv(res_single, "exposure/res_single_ptsd.csv")
 
 
-out_raw <- fread("sumstats/Insomnia.txt")
+out_raw <- fread("data/Insomnia.txt")
 out_raw <- as.data.frame(out_raw)
 out_raw$BETA <-  (exp(out_raw$OR)/(1+exp(out_raw$OR)))
 
@@ -122,14 +131,14 @@ out_dat <- format_data( out_raw,
                 snp_col = "RSID_UKB",
                 beta_col = "BETA",
                 se_col = "SE",
-                effect_allele_col = "A2",
-                other_allele_col = "A1",
+                effect_allele_col = "A1",
+                other_allele_col = "A2",
                 pval_col = "P",
                 chr_col = "CHR",
                 pos_col = "BP"
 )
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -137,42 +146,41 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "exposure/mr_dat_insomnia")
 write.csv(mr_res, "exposure/mr_res_insomnia.csv")
 write.csv(res_single, "exposure/res_single_insomnia.csv")
 
-out_raw <- fread("sumstats/COVID.txt")
-out_raw <- as.data.frame(out_raw)
+# out_raw <- fread("data/COVID.txt")
+# out_raw <- as.data.frame(out_raw)
+# 
+# out_dat <- format_data( out_raw,
+#             type = "outcome",
+#             snp_col = "rsid",
+#             beta_col = "all_inv_var_meta_beta",
+#             se_col = "all_inv_var_meta_sebeta",
+#             effect_allele_col = "REF",
+#             other_allele_col = "ALT",
+#             pval_col = "all_inv_var_meta_p",
+#             # chr_col = "#CHR",
+#             pos_col = "POS"
+# )
+# 
+# harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+# 
+# mr_res <- mr(harmonized_data, method_list = c(
+#   "mr_ivw_fe", "mr_ivw_mre"
+# ))
+# res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
+# 
+# mr_res
+# res_single
+# 
+# write.csv(harmonized_data, "exposure/mr_dat_covid")
+# write.csv(mr_res, "exposure/mr_res_covid.csv")
+# write.csv(res_single, "exposure/res_single_covid.csv")
 
-out_dat <- format_data( out_raw,
-            type = "outcome",
-            snp_col = "rsid",
-            beta_col = "all_inv_var_meta_beta",
-            se_col = "all_inv_var_meta_sebeta",
-            effect_allele_col = "ALT",
-            other_allele_col = "REF",
-            pval_col = "all_inv_var_meta_p",
-            # chr_col = "#CHR",
-            pos_col = "POS"
-)
-
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
-
-mr_res <- mr(harmonized_data, method_list = c(
-  "mr_ivw_fe", "mr_ivw_mre"
-))
-res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
-
-mr_res
-res_single
-
-write.csv(harmonized_data, "exposure/mr_dat_covid")
-write.csv(mr_res, "exposure/mr_res_covid.csv")
-write.csv(res_single, "exposure/res_single_covid.csv")
-
-out_raw <- fread("sumstats/Chronotype.txt")
+out_raw <- fread("data/Chronotype.txt")
 out_raw <- as.data.frame(out_raw)
 
 out_dat <- format_data( out_raw,
@@ -187,7 +195,7 @@ out_dat <- format_data( out_raw,
           pos_col = "BP"
 )
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -195,8 +203,45 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "exposure/mr_dat_chrono")
 write.csv(mr_res, "exposure/mr_res_chrono.csv")
 write.csv(res_single, "exposure/res_single_chrono.csv")
+
+#################################
+
+
+out_raw <- fread("data/Number_of_Children_Ever_Born.txt")
+out_raw <- as.data.frame(out_raw)
+
+out_raw$BETA = out_raw$Zscore / sqrt(2*out_raw$Freq_HapMap*(1 - out_raw$Freq_HapMap)*(318463+ out_raw$Zscore^2))
+out_raw$SE = 1 / sqrt(2*out_raw$Freq_HapMap*(1 - out_raw$Freq_HapMap)*(318463 + out_raw$Zscore^2))
+
+
+out_dat <- format_data( out_raw,
+                        type = "outcome",
+                        snp_col = "SNPID",
+                        beta_col = "BETA",
+                        se_col = "SE",
+                        effect_allele_col = "A1",
+                        other_allele_col = "A2",
+                        pval_col = "Pvalue",
+                        chr_col = "CHR",
+                        pos_col = "BP"
+)
+
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
+
+mr_res <- mr(harmonized_data, method_list = c(
+  "mr_ivw_fe", "mr_ivw_mre"
+))
+res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
+
+mr_res
+
+write.csv(harmonized_data, "exposure/mr_dat_number")
+write.csv(mr_res, "exposure/mr_res_number.csv")
+write.csv(res_single, "exposure/res_single_number.csv")
+
+
+
--- sm_MR.R
+++ sm_MR.R
@@ -4,7 +4,7 @@ library(tidyverse)    # Data wrangling
 library(TwoSampleMR)  # MR 
 library(LDlinkR)      # LD and proxy snps
 
-out_raw <- fread("sumstats/SM_EUR_QCed_v2.txt")
+out_raw <- fread("data/SM_EUR_QCed_v2.txt")
 out_raw <- as.data.frame(out_raw)
 
 out_dat <- format_data( out_raw,
@@ -12,16 +12,15 @@ out_dat <- format_data( out_raw,
                         snp_col = "rsID",
                         beta_col = "BETA",
                         se_col = "SE",
-                        effect_allele_col = "A2",
-                        other_allele_col = "A1",
+                        effect_allele_col = "A1",
+                        other_allele_col = "A2",
                         pval_col = "P",
                         chr_col = "CHR",
                         pos_col = "POS"
 )
 
 
-exp_raw <- fread("sumstats/Age_at_first_birth.tsv")
-exp_raw <- subset(exp_raw,exp_raw$p_value<1e-05)
+exp_raw <- fread("data/Age_at_first_birth.tsv")
 exp_raw <- as.data.frame(exp_raw)
 
 exp_dat <- format_data( exp_raw,
@@ -33,10 +32,21 @@ exp_dat <- format_data( exp_raw,
                         other_allele_col = "other_allele",
                         pval_col = "p_value"
 )
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
 
+# Perform LD clumping on SNP data, filter SNPs to make it run faster
+clumped_exp <- exp_dat %>% 
+  filter(pval.exposure < 0.01) %>%
+  clump_data(.,
+             clump_kb = 10000,
+             clump_r2 = 0.001,
+             clump_p1 = 1,
+             clump_p2 = 1,
+             pop = "EUR"
+  )
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -44,15 +54,15 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "outcome/mr_dat_age")
 write.csv(mr_res, "outcome/mr_res_age.csv")
 write.csv(res_single, "outcome/res_single_age.csv")
 
-exp_raw <- fread("sumstats/Income.txt")
+exp_raw <- fread("data/Income.txt")
 exp_raw <- as.data.frame(exp_raw)
-exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
+# exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
+
 exp_dat <- format_data( exp_raw,
                         type = "exposure",
                         snp_col = "SNP",
@@ -64,10 +74,23 @@ exp_dat <- format_data( exp_raw,
                         chr_col = "Chr", 
                         pos_col = "BPos"
 )
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+clumped_exp <- exp_dat %>% 
+  filter(pval.exposure < 0.01) %>%
+  clump_data(.,
+             clump_kb = 10000,
+             clump_r2 = 0.001,
+             clump_p1 = 1,
+             clump_p2 = 1,
+             pop = "EUR"
+  )
+filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+
+exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
+
+# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
 
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -75,15 +98,14 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "outcome/mr_dat_income")
 write.csv(mr_res, "outcome/mr_res_income.csv")
 write.csv(res_single, "outcome/res_single_income.csv")
 
 
-exp_raw <- fread("sumstats/Post-Traumatic_Stress_Disorder.results")
-exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
+exp_raw <- fread("data/Post-Traumatic_Stress_Disorder.results")
+# exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
 exp_raw <- as.data.frame(exp_raw)
 exp_raw$BETA <-  (exp(exp_raw$OR)/(1+exp(exp_raw$OR)))
 
@@ -92,15 +114,29 @@ exp_dat <- format_data( exp_raw,
                         snp_col = "SNP",
                         beta_col = "BETA",
                         se_col = "SE",
-                        effect_allele_col = "A2",
-                        other_allele_col = "A1",
+                        effect_allele_col = "A1",
+                        other_allele_col = "A2",
                         pval_col = "P",
                         chr_col = "CHR",
                         pos_col = "BP"
 )
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+clumped_exp <- exp_dat %>% 
+  filter(pval.exposure < 0.01) %>%
+  clump_data(.,
+             clump_kb = 10000,
+             clump_r2 = 0.001,
+             clump_p1 = 1,
+             clump_p2 = 1,
+             pop = "EUR"
+  )
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+exposure_dat <- filter(clumped_exp, pval.exposure < 1e-05)
+
+# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+
+
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -108,80 +144,101 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "outcome/mr_dat_ptsd")
 write.csv(mr_res, "outcome/mr_res_ptsd.csv")
 write.csv(res_single, "outcome/res_single_ptsd.csv")
 
 
-exp_raw <- fread("sumstats/Insomnia.txt")
-exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
-exp_raw <- as.data.frame(exp_raw)
-exp_raw$BETA <-  (exp(exp_raw$OR)/(1+exp(exp_raw$OR)))
+# exp_raw <- fread("data/Insomnia.txt")
+# # exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
+# exp_raw <- as.data.frame(exp_raw)
+# exp_raw$BETA <-  log(exp_raw$OR)
+# 
+# exp_dat <- format_data( exp_raw,
+#                         type = "exposure",
+#                         snp_col = "RSID_UKB",
+#                         beta_col = "BETA",
+#                         se_col = "SE",
+#                         effect_allele_col = "A1",
+#                         other_allele_col = "A2",
+#                         pval_col = "P",
+#                         chr_col = "CHR",
+#                         pos_col = "BP"
+# )
+# clumped_exp <- exp_dat %>% 
+#   filter(pval.exposure < 0.01) %>%
+#   clump_data(.,
+#              clump_kb = 10000,
+#              clump_r2 = 0.001,
+#              clump_p1 = 1,
+#              clump_p2 = 1,
+#              pop = "EUR"
+#   )
+# filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+# exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
+# 
+# # clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+# 
+# 
+# harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
+# 
+# mr_res <- mr(harmonized_data, method_list = c(
+#   "mr_ivw_fe", "mr_ivw_mre"
+# ))
+# res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
+# 
+# mr_res
+# 
+# write.csv(harmonized_data, "outcome/mr_dat_insomnia")
+# write.csv(mr_res, "outcome/mr_res_insomnia.csv")
+# write.csv(res_single, "outcome/res_single_insomnia.csv")
 
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "RSID_UKB",
-                        beta_col = "BETA",
-                        se_col = "SE",
-                        effect_allele_col = "A2",
-                        other_allele_col = "A1",
-                        pval_col = "P",
-                        chr_col = "CHR",
-                        pos_col = "BP"
-)
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+# exp_raw <- fread("data/COVID.txt")
+# # exp_raw <- subset(exp_raw,exp_raw$all_inv_var_meta_p<1e-05)
+# exp_raw <- as.data.frame(exp_raw)
+# 
+# exp_dat <- format_data( exp_raw,
+#                         type = "exposure",
+#                         snp_col = "rsid",
+#                         beta_col = "all_inv_var_meta_beta",
+#                         se_col = "all_inv_var_meta_sebeta",
+#                         effect_allele_col = "REF",
+#                         other_allele_col = "ALT",
+#                         pval_col = "all_inv_var_meta_p",
+#                         # chr_col = "#CHR",
+#                         pos_col = "POS"
+# )
+# clumped_exp <- exp_dat %>% 
+#   filter(pval.exposure < 0.01) %>%
+#   clump_data(.,
+#              clump_kb = 10000,
+#              clump_r2 = 0.001,
+#              clump_p1 = 1,
+#              clump_p2 = 1,
+#              pop = "EUR"
+#   )
+# filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+# exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
+# 
+# # clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+# 
+# 
+# harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
+# 
+# mr_res <- mr(harmonized_data, method_list = c(
+#   "mr_ivw_fe", "mr_ivw_mre"
+# ))
+# res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
+# 
+# mr_res
+# 
+# write.csv(harmonized_data, "outcome/mr_dat_covid")
+# write.csv(mr_res, "outcome/mr_res_covid.csv")
+# write.csv(res_single, "outcome/res_single_covid.csv")
 
-
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
-
-mr_res <- mr(harmonized_data, method_list = c(
-  "mr_ivw_fe", "mr_ivw_mre"
-))
-res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
-
-mr_res
-res_single
-
-write.csv(harmonized_data, "outcome/mr_dat_insomnia")
-write.csv(mr_res, "outcome/mr_res_insomnia.csv")
-write.csv(res_single, "outcome/res_single_insomnia.csv")
-
-exp_raw <- fread("sumstats/COVID.txt")
-exp_raw <- subset(exp_raw,exp_raw$all_inv_var_meta_p<1e-05)
-exp_raw <- as.data.frame(exp_raw)
-
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "rsid",
-                        beta_col = "all_inv_var_meta_beta",
-                        se_col = "all_inv_var_meta_sebeta",
-                        effect_allele_col = "ALT",
-                        other_allele_col = "REF",
-                        pval_col = "all_inv_var_meta_p",
-                        # chr_col = "#CHR",
-                        pos_col = "POS"
-)
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
-
-
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
-
-mr_res <- mr(harmonized_data, method_list = c(
-  "mr_ivw_fe", "mr_ivw_mre"
-))
-res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
-
-mr_res
-res_single
-
-write.csv(harmonized_data, "outcome/mr_dat_covid")
-write.csv(mr_res, "outcome/mr_res_covid.csv")
-write.csv(res_single, "outcome/res_single_covid.csv")
-
-exp_raw <- fread("sumstats/Chronotype.txt")
-exp_raw <- subset(exp_raw,exp_raw$P_BOLT_LMM<1e-05)
+exp_raw <- fread("data/Chronotype.txt")
+# exp_raw <- subset(exp_raw,exp_raw$P_BOLT_LMM<1e-05)
 exp_raw <- as.data.frame(exp_raw)
 
 exp_dat <- format_data( exp_raw,
@@ -195,10 +252,22 @@ exp_dat <- format_data( exp_raw,
                         chr_col = "CHR",
                         pos_col = "BP"
 )
-clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+clumped_exp <- exp_dat %>% 
+  filter(pval.exposure < 0.01) %>%
+  clump_data(.,
+             clump_kb = 10000,
+             clump_r2 = 0.001,
+             clump_p1 = 1,
+             clump_p2 = 1,
+             pop = "EUR"
+  )
+filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
+
+# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
 
 
-harmonized_data <- harmonise_data(clumped_exp,out_dat,action=1)
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
@@ -206,8 +275,58 @@ mr_res <- mr(harmonized_data, method_list = c(
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
-res_single
 
 write.csv(harmonized_data, "outcome/mr_dat_chrono")
 write.csv(mr_res, "outcome/mr_res_chrono.csv")
 write.csv(res_single, "outcome/res_single_chrono.csv")
+
+######################################
+
+
+exp_raw <- fread("data/Number_of_Children_Ever_Born.txt")
+# exp_raw <- subset(exp_raw,exp_raw$Pvalue<1e-05)
+exp_raw <- as.data.frame(exp_raw)
+
+exp_raw$BETA = exp_raw$Zscore / sqrt(2*exp_raw$Freq_HapMap*(1 - exp_raw$Freq_HapMap)*(318463+ exp_raw$Zscore^2))
+exp_raw$SE = 1 / sqrt(2*exp_raw$Freq_HapMap*(1 - exp_raw$Freq_HapMap)*(318463 + exp_raw$Zscore^2))
+
+
+exp_dat <- format_data( exp_raw,
+                        type = "exposure",
+                        snp_col = "SNPID",
+                        beta_col = "BETA",
+                        se_col = "SE",
+                        effect_allele_col = "A1",
+                        other_allele_col = "A2",
+                        pval_col = "Pvalue",
+                        chr_col = "CHR",
+                        pos_col = "BP"
+)
+
+clumped_exp <- exp_dat %>% 
+  filter(pval.exposure < 0.01) %>%
+  clump_data(.,
+             clump_kb = 10000,
+             clump_r2 = 0.001,
+             clump_p1 = 1,
+             clump_p2 = 1,
+             pop = "EUR"
+  )
+filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+exposure_dat <- filter(clumped_exp, pval.exposure < 1e-05)
+
+# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+
+
+harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
+mr_res <- mr(harmonized_data, method_list = c(
+  "mr_ivw_fe", "mr_ivw_mre"
+))
+res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
+
+mr_res
+
+write.csv(harmonized_data, "outcome/mr_dat_number")
+write.csv(mr_res, "outcome/mr_res_number.csv")
+write.csv(res_single, "outcome/res_single_number.csv")
+

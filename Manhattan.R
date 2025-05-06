925e8f5d33212f0b89ffba9215f1b42a8f99a379
Rutuja Gupte <rmgupte@wisc.edu>
2025-05-05 22:00:20 -0500

    cleaned and commented code

--- Manhattan.R
+++ Manhattan.R
@@ -1,18 +1,12 @@
+# Adapted from course files
+
 library(qqman)
 library(data.table)
 library(tidyverse)
 
-gwas <- as.data.frame(fread("data/SM_EUR_QCed_v2.txt",header=T))
+gwas <- as.data.frame(fread("data/SM_EUR_QCed_v2.txt", header = T))
 head(gwas)
 
-# gwas$P <- gwas$`P-value`
-# gwas$CHR <- str_split_i(gwas$MarkerName, ":", 1)
-# gwas$CHR <- ifelse(gwas$CHR=="X", 23, gwas$CHR)
-# gwas$CHR <- ifelse(gwas$CHR=="Y", 24, gwas$CHR)
-# gwas$CHR <- ifelse(gwas$CHR=="MT", 25, gwas$CHR)
-# gwas$CHR <- as.numeric(gwas$CHR)
-# gwas$BP <- as.numeric(str_split_i(gwas$MarkerName, ":", 2))
-# gwas$SNP <- gwas$MarkerName
 gwas$BP <- gwas$POS
 gwas$SNP <- gwas$rsID
 head(gwas)
@@ -21,75 +15,14 @@ interest <- gwas %>% filter(P < 5e-08)
 snpsOfInterest <- interest$SNP
 
 # make manhattan plot
-png("manhattan_final.png",units="in",width = 15,height = 8,res=300)
-manhattan(gwas,chr="CHR",bp="BP",p="P",snp="SNP",suggestiveline=F,genomewideline=-log10(5e-08), annotatePval=5e-08, highlight = snpsOfInterest)
+png("manhattan_final.png", units = "in", width = 15, height = 8, res = 300)
+manhattan(gwas, chr = "CHR", bp = "BP", p = "P", snp = "SNP", suggestiveline = F, genomewideline = -log10(5e-08), annotatePval = 5e-08, highlight = snpsOfInterest)
 dev.off()
 
 # make QQ plot
-png("qqplot_final.png",units="in",width=8,height=8,res=300)
+png("qqplot_final.png", units = "in", width = 8, height = 8, res = 300)
 qq(gwas$P)
 dev.off()
 
 # print genomic inflation factor
-(lambda=median((qnorm(gwas$P/2,0,1))^2)/qchisq(0.5,1))
-
-
-# gwas <- as.data.frame(fread("SM_transethnic_sumstats_unfiltered.txt",header=T))
-# gwas <- gwas %>% filter(Ncohort>=11)
-# head(gwas)
-# 
-# gwas$P <- gwas$`P-value_association`
-# gwas$CHR <- str_split_i(gwas$MarkerName, ":", 1)
-# # gwas$CHR <- ifelse(gwas$CHR=="X", 23, gwas$CHR)
-# # gwas$CHR <- ifelse(gwas$CHR=="Y", 24, gwas$CHR)
-# # gwas$CHR <- ifelse(gwas$CHR=="MT", 25, gwas$CHR)
-# gwas$CHR <- as.numeric(gwas$Chromosome)
-# gwas$BP <- as.numeric(gwas$Position)
-# gwas$SNP <- gwas$MarkerName
-# head(gwas)
-# 
-# gwas %>% filter(P < 5e-08)
-# 
-# # make manhattan plot
-# png("manhattan_trans.png",units="in",width = 15,height = 8,res=300)
-# manhattan(gwas,chr="CHR",bp="BP",p="P",snp="SNP",suggestiveline=F,genomewideline=-log10(5e-08))
-# dev.off()
-# 
-# # make QQ plot
-# png("qqplot_trans.png",units="in",width=8,height=8,res=300)
-# qq(gwas$P)
-# dev.off()
-# 
-# # print genomic inflation factor
-# (lambda=median((qnorm(gwas$P/2,0,1))^2)/qchisq(0.5,1))
-# 
-# 
-# 
-# gwas <- as.data.frame(fread("RM_sumstats_unfiltered.txt",header=T))
-# gwas <- gwas %>% filter(HetDf>=1) %>% filter(Freq1>0.005) %>% filter(MinFreq>0.001)
-# head(gwas)
-# 
-# gwas$P <- gwas$`P-value`
-# gwas$CHR <- str_split_i(gwas$MarkerName, ":", 1)
-# gwas$CHR <- ifelse(gwas$CHR=="X", 23, gwas$CHR)
-# gwas$CHR <- ifelse(gwas$CHR=="Y", 24, gwas$CHR)
-# gwas$CHR <- ifelse(gwas$CHR=="MT", 25, gwas$CHR)
-# gwas$CHR <- as.numeric(gwas$CHR)
-# gwas$BP <- as.numeric(str_split_i(gwas$MarkerName, ":", 2))
-# gwas$SNP <- gwas$MarkerName
-# head(gwas)
-# 
-# gwas %>% filter(P < 5e-08)
-# 
-# # make manhattan plot
-# png("manhattan_rm.png",units="in",width = 15,height = 8,res=300)
-# manhattan(gwas,chr="CHR",bp="BP",p="P",snp="SNP",suggestiveline=F,genomewideline=-log10(5e-08))
-# dev.off()
-# 
-# # make QQ plot
-# png("qqplot_rm.png",units="in",width=8,height=8,res=300)
-# qq(gwas$P)
-# dev.off()
-# 
-# # print genomic inflation factor
-# (lambda=median((qnorm(gwas$P/2,0,1))^2)/qchisq(0.5,1))
+(lambda <- median((qnorm(gwas$P / 2, 0, 1))^2) / qchisq(0.5, 1))
--- sm_MR.R
+++ sm_MR.R
@@ -1,56 +1,67 @@
+# Code adapted from https://andrewslabucsf.github.io/MR-tutorial/
+
 library(data.table)
 library(tidyverse)
-library(tidyverse)    # Data wrangling 
-library(TwoSampleMR)  # MR 
-library(LDlinkR)      # LD and proxy snps
+library(tidyverse) # Data wrangling
+library(TwoSampleMR) # MR
+library(LDlinkR) # LD and proxy snps
 
+# Reading file
 out_raw <- fread("data/SM_EUR_QCed_v2.txt")
 out_raw <- as.data.frame(out_raw)
 
-out_dat <- format_data( out_raw,
-                        type = "outcome",
-                        snp_col = "rsID",
-                        beta_col = "BETA",
-                        se_col = "SE",
-                        effect_allele_col = "A1",
-                        other_allele_col = "A2",
-                        pval_col = "P",
-                        chr_col = "CHR",
-                        pos_col = "POS"
+# Format output
+out_dat <- format_data(out_raw,
+  type = "outcome",
+  snp_col = "rsID",
+  beta_col = "BETA",
+  se_col = "SE",
+  effect_allele_col = "A1",
+  other_allele_col = "A2",
+  pval_col = "P",
+  chr_col = "CHR",
+  pos_col = "POS"
 )
 
 
+# Reading file
 exp_raw <- fread("data/Age_at_first_birth.tsv")
 exp_raw <- as.data.frame(exp_raw)
 
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "variant_id",
-                        beta_col = "beta",
-                        se_col = "standard_error",
-                        effect_allele_col = "effect_allele",
-                        other_allele_col = "other_allele",
-                        pval_col = "p_value"
+exp_dat <- format_data(exp_raw,
+  type = "exposure",
+  snp_col = "variant_id",
+  beta_col = "beta",
+  se_col = "standard_error",
+  effect_allele_col = "effect_allele",
+  other_allele_col = "other_allele",
+  pval_col = "p_value"
 )
 
-# Perform LD clumping on SNP data, filter SNPs to make it run faster
-clumped_exp <- exp_dat %>% 
+# LD pruning
+clumped_exp <- exp_dat %>%
   filter(pval.exposure < 0.01) %>%
   clump_data(.,
-             clump_kb = 10000,
-             clump_r2 = 0.001,
-             clump_p1 = 1,
-             clump_p2 = 1,
-             pop = "EUR"
+    clump_kb = 10000,
+    clump_r2 = 0.001,
+    clump_p1 = 1,
+    clump_p2 = 1,
+    pop = "EUR"
   )
 
 filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
-exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
-harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
 
+# Filter to significant SNPs
+exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
+
+harmonized_data <- harmonise_data(exposure_dat, out_dat, action = 1)
+
+# Mendelian Randomization
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
 ))
+
+# Leave one marker out - sensitivity analysis
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
@@ -59,44 +70,47 @@ write.csv(harmonized_data, "outcome/mr_dat_age")
 write.csv(mr_res, "outcome/mr_res_age.csv")
 write.csv(res_single, "outcome/res_single_age.csv")
 
+# Reading file
 exp_raw <- fread("data/Income.txt")
 exp_raw <- as.data.frame(exp_raw)
-# exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
 
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "SNP",
-                        beta_col = "Beta",
-                        se_col = "Standard_Error_of_Beta",
-                        effect_allele_col = "Effect_Allele",
-                        other_allele_col = "Non_effect_Allele",
-                        pval_col = "P",
-                        chr_col = "Chr", 
-                        pos_col = "BPos"
+# Format exposure
+exp_dat <- format_data(exp_raw,
+  type = "exposure",
+  snp_col = "SNP",
+  beta_col = "Beta",
+  se_col = "Standard_Error_of_Beta",
+  effect_allele_col = "Effect_Allele",
+  other_allele_col = "Non_effect_Allele",
+  pval_col = "P",
+  chr_col = "Chr",
+  pos_col = "BPos"
 )
-clumped_exp <- exp_dat %>% 
+
+# LD pruning
+clumped_exp <- exp_dat %>%
   filter(pval.exposure < 0.01) %>%
   clump_data(.,
-             clump_kb = 10000,
-             clump_r2 = 0.001,
-             clump_p1 = 1,
-             clump_p2 = 1,
-             pop = "EUR"
+    clump_kb = 10000,
+    clump_r2 = 0.001,
+    clump_p1 = 1,
+    clump_p2 = 1,
+    pop = "EUR"
   )
 filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
 
+# Filter to significant SNPs
 exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
 
-# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+harmonized_data <- harmonise_data(exposure_dat, out_dat, action = 1)
 
-
-harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
-
+# Mendelian Randomization
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
 ))
-res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
+# Leave one marker out - sensitivity analysis
+res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 mr_res
 
 write.csv(harmonized_data, "outcome/mr_dat_income")
@@ -104,43 +118,48 @@ write.csv(mr_res, "outcome/mr_res_income.csv")
 write.csv(res_single, "outcome/res_single_income.csv")
 
 
+# Reading file
 exp_raw <- fread("data/Post-Traumatic_Stress_Disorder.results")
-# exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
 exp_raw <- as.data.frame(exp_raw)
-exp_raw$BETA <-  (exp(exp_raw$OR)/(1+exp(exp_raw$OR)))
+exp_raw$BETA <- (exp(exp_raw$OR) / (1 + exp(exp_raw$OR)))
 
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "SNP",
-                        beta_col = "BETA",
-                        se_col = "SE",
-                        effect_allele_col = "A1",
-                        other_allele_col = "A2",
-                        pval_col = "P",
-                        chr_col = "CHR",
-                        pos_col = "BP"
+# Format exposure
+exp_dat <- format_data(exp_raw,
+  type = "exposure",
+  snp_col = "SNP",
+  beta_col = "BETA",
+  se_col = "SE",
+  effect_allele_col = "A1",
+  other_allele_col = "A2",
+  pval_col = "P",
+  chr_col = "CHR",
+  pos_col = "BP"
 )
-clumped_exp <- exp_dat %>% 
+
+# LD pruning
+clumped_exp <- exp_dat %>%
   filter(pval.exposure < 0.01) %>%
   clump_data(.,
-             clump_kb = 10000,
-             clump_r2 = 0.001,
-             clump_p1 = 1,
-             clump_p2 = 1,
-             pop = "EUR"
+    clump_kb = 10000,
+    clump_r2 = 0.001,
+    clump_p1 = 1,
+    clump_p2 = 1,
+    pop = "EUR"
   )
 
 filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+
+# Filter to significant SNPs
 exposure_dat <- filter(clumped_exp, pval.exposure < 1e-05)
 
-# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+harmonized_data <- harmonise_data(exposure_dat, out_dat, action = 1)
 
-
-harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
-
+# Mendelian Randomization
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
 ))
+
+# Leave one marker out - sensitivity analysis
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
@@ -149,129 +168,46 @@ write.csv(harmonized_data, "outcome/mr_dat_ptsd")
 write.csv(mr_res, "outcome/mr_res_ptsd.csv")
 write.csv(res_single, "outcome/res_single_ptsd.csv")
 
-
-# exp_raw <- fread("data/Insomnia.txt")
-# # exp_raw <- subset(exp_raw,exp_raw$P<1e-05)
-# exp_raw <- as.data.frame(exp_raw)
-# exp_raw$BETA <-  log(exp_raw$OR)
-# 
-# exp_dat <- format_data( exp_raw,
-#                         type = "exposure",
-#                         snp_col = "RSID_UKB",
-#                         beta_col = "BETA",
-#                         se_col = "SE",
-#                         effect_allele_col = "A1",
-#                         other_allele_col = "A2",
-#                         pval_col = "P",
-#                         chr_col = "CHR",
-#                         pos_col = "BP"
-# )
-# clumped_exp <- exp_dat %>% 
-#   filter(pval.exposure < 0.01) %>%
-#   clump_data(.,
-#              clump_kb = 10000,
-#              clump_r2 = 0.001,
-#              clump_p1 = 1,
-#              clump_p2 = 1,
-#              pop = "EUR"
-#   )
-# filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
-# exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
-# 
-# # clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
-# 
-# 
-# harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
-# 
-# mr_res <- mr(harmonized_data, method_list = c(
-#   "mr_ivw_fe", "mr_ivw_mre"
-# ))
-# res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
-# 
-# mr_res
-# 
-# write.csv(harmonized_data, "outcome/mr_dat_insomnia")
-# write.csv(mr_res, "outcome/mr_res_insomnia.csv")
-# write.csv(res_single, "outcome/res_single_insomnia.csv")
-
-# exp_raw <- fread("data/COVID.txt")
-# # exp_raw <- subset(exp_raw,exp_raw$all_inv_var_meta_p<1e-05)
-# exp_raw <- as.data.frame(exp_raw)
-# 
-# exp_dat <- format_data( exp_raw,
-#                         type = "exposure",
-#                         snp_col = "rsid",
-#                         beta_col = "all_inv_var_meta_beta",
-#                         se_col = "all_inv_var_meta_sebeta",
-#                         effect_allele_col = "REF",
-#                         other_allele_col = "ALT",
-#                         pval_col = "all_inv_var_meta_p",
-#                         # chr_col = "#CHR",
-#                         pos_col = "POS"
-# )
-# clumped_exp <- exp_dat %>% 
-#   filter(pval.exposure < 0.01) %>%
-#   clump_data(.,
-#              clump_kb = 10000,
-#              clump_r2 = 0.001,
-#              clump_p1 = 1,
-#              clump_p2 = 1,
-#              pop = "EUR"
-#   )
-# filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
-# exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
-# 
-# # clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
-# 
-# 
-# harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
-# 
-# mr_res <- mr(harmonized_data, method_list = c(
-#   "mr_ivw_fe", "mr_ivw_mre"
-# ))
-# res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
-# 
-# mr_res
-# 
-# write.csv(harmonized_data, "outcome/mr_dat_covid")
-# write.csv(mr_res, "outcome/mr_res_covid.csv")
-# write.csv(res_single, "outcome/res_single_covid.csv")
-
+# Reading file
 exp_raw <- fread("data/Chronotype.txt")
-# exp_raw <- subset(exp_raw,exp_raw$P_BOLT_LMM<1e-05)
 exp_raw <- as.data.frame(exp_raw)
 
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "SNP",
-                        beta_col = "BETA",
-                        se_col = "SE",
-                        effect_allele_col = "ALLELE1",
-                        other_allele_col = "ALLELE0",
-                        pval_col = "P_BOLT_LMM",
-                        chr_col = "CHR",
-                        pos_col = "BP"
+# Format exposure
+exp_dat <- format_data(exp_raw,
+  type = "exposure",
+  snp_col = "SNP",
+  beta_col = "BETA",
+  se_col = "SE",
+  effect_allele_col = "ALLELE1",
+  other_allele_col = "ALLELE0",
+  pval_col = "P_BOLT_LMM",
+  chr_col = "CHR",
+  pos_col = "BP"
 )
-clumped_exp <- exp_dat %>% 
+
+# LD pruning
+clumped_exp <- exp_dat %>%
   filter(pval.exposure < 0.01) %>%
   clump_data(.,
-             clump_kb = 10000,
-             clump_r2 = 0.001,
-             clump_p1 = 1,
-             clump_p2 = 1,
-             pop = "EUR"
+    clump_kb = 10000,
+    clump_r2 = 0.001,
+    clump_p1 = 1,
+    clump_p2 = 1,
+    pop = "EUR"
   )
 filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+
+# Filter to significant SNPs
 exposure_dat <- filter(clumped_exp, pval.exposure < 5e-08)
 
-# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+harmonized_data <- harmonise_data(exposure_dat, out_dat, action = 1)
 
-
-harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
-
+# Mendelian Randomization
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
 ))
+
+# Leave one marker out - sensitivity analysis
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
@@ -280,48 +216,50 @@ write.csv(harmonized_data, "outcome/mr_dat_chrono")
 write.csv(mr_res, "outcome/mr_res_chrono.csv")
 write.csv(res_single, "outcome/res_single_chrono.csv")
 
-######################################
 
-
+# Reading file
 exp_raw <- fread("data/Number_of_Children_Ever_Born.txt")
-# exp_raw <- subset(exp_raw,exp_raw$Pvalue<1e-05)
 exp_raw <- as.data.frame(exp_raw)
 
-exp_raw$BETA = exp_raw$Zscore / sqrt(2*exp_raw$Freq_HapMap*(1 - exp_raw$Freq_HapMap)*(318463+ exp_raw$Zscore^2))
-exp_raw$SE = 1 / sqrt(2*exp_raw$Freq_HapMap*(1 - exp_raw$Freq_HapMap)*(318463 + exp_raw$Zscore^2))
+exp_raw$BETA <- exp_raw$Zscore / sqrt(2 * exp_raw$Freq_HapMap * (1 - exp_raw$Freq_HapMap) * (318463 + exp_raw$Zscore^2))
+exp_raw$SE <- 1 / sqrt(2 * exp_raw$Freq_HapMap * (1 - exp_raw$Freq_HapMap) * (318463 + exp_raw$Zscore^2))
 
-
-exp_dat <- format_data( exp_raw,
-                        type = "exposure",
-                        snp_col = "SNPID",
-                        beta_col = "BETA",
-                        se_col = "SE",
-                        effect_allele_col = "A1",
-                        other_allele_col = "A2",
-                        pval_col = "Pvalue",
-                        chr_col = "CHR",
-                        pos_col = "BP"
+# Format exposure
+exp_dat <- format_data(exp_raw,
+  type = "exposure",
+  snp_col = "SNPID",
+  beta_col = "BETA",
+  se_col = "SE",
+  effect_allele_col = "A1",
+  other_allele_col = "A2",
+  pval_col = "Pvalue",
+  chr_col = "CHR",
+  pos_col = "BP"
 )
 
-clumped_exp <- exp_dat %>% 
+# LD pruning
+clumped_exp <- exp_dat %>%
   filter(pval.exposure < 0.01) %>%
   clump_data(.,
-             clump_kb = 10000,
-             clump_r2 = 0.001,
-             clump_p1 = 1,
-             clump_p2 = 1,
-             pop = "EUR"
+    clump_kb = 10000,
+    clump_r2 = 0.001,
+    clump_p1 = 1,
+    clump_p2 = 1,
+    pop = "EUR"
   )
 filter(clumped_exp, pval.exposure < 5e-08) %>% nrow()
+
+# Filter to significant SNPs
 exposure_dat <- filter(clumped_exp, pval.exposure < 1e-05)
 
-# clumped_exp <- clump_data(exp_dat,clump_r2=0.01,pop="EUR") 
+harmonized_data <- harmonise_data(exposure_dat, out_dat, action = 1)
 
-
-harmonized_data <- harmonise_data(exposure_dat,out_dat,action=1)
+# Mendelian Randomization
 mr_res <- mr(harmonized_data, method_list = c(
   "mr_ivw_fe", "mr_ivw_mre"
 ))
+
+# Leave one marker out - sensitivity analysis
 res_single <- mr_singlesnp(harmonized_data, all_method = c("mr_ivw_fe")) %>% as_tibble()
 
 mr_res
@@ -329,4 +267,3 @@ mr_res
 write.csv(harmonized_data, "outcome/mr_dat_number")
 write.csv(mr_res, "outcome/mr_res_number.csv")
 write.csv(res_single, "outcome/res_single_number.csv")
-
